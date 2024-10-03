7aad8fa87e8d7e2fb3b31f86d5cd3c6ef8e5a055
Baejita <homokoro@gmail.com>
2024-10-03 23:08:58 +0700

    animation

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import { createPortal } from "react-dom";
+motion;
+export default function Modal({ title, children, onClose }) {
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+          exit: { opacity: 0, y: 30 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        open
+        className="modal"
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById("modal")
+  );
+}
